3ede7264dd2c8c7842ea61b7e352f79059da244f
Muhammad Abu Bakar <31399732+Muhammadabuabakr@users.noreply.github.com>
2021-10-12 13:02:57 +0500

    Update Inheritance.cpp

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -80,3 +80,172 @@ int main(){
 		
 	}
 }
+
+//Question#2
+
+#include<iostream>
+#include<cmath>
+#include<math.h>
+using namespace std;
+class polygon{
+public:
+	float len;
+	float wid;
+	
+	polygon(){
+		cout << "Enter the value of length:";
+		cin >> len;
+		cout << "Enter the value of width:";
+		cin >> wid;
+	}
+	virtual float Area()=0;
+	virtual float Perimeter()=0;
+	virtual float Display()=0;	
+};
+class square:public polygon
+{
+public:
+	float Area(){
+		cout <<endl;
+		cout <<"Area of square:"<<4*len<<endl;	
+	}
+	float Perimeter(){
+		
+		cout <<"Perimeter of square:"<<4*len<<endl;
+		cout <<endl;
+	}
+	float Display(){
+		Area();
+		Perimeter();
+	}
+};
+class Rectangle:public polygon
+{
+	float Area(){
+		cout << endl;
+		cout <<"Area of Rectangle:"<<2*len*wid<<endl;
+	}
+	float Perimeter(){
+		cout <<"Perimeter of Rectangle:"<<2*(len+wid)<<endl;
+		cout << endl;
+	}
+	float Display(){
+		Area();
+		Perimeter();
+	}
+};
+class Triangle:public polygon
+{
+	float Area(){
+		cout << endl;
+		cout <<"Area of Triangle:"<<(len*wid)/2<<endl;
+	}
+	float Perimeter(){
+		float height;
+		height = sqrt(pow(len,2) + pow(wid,2));
+		cout <<"Perimeter of Triangle:"<<len+wid+height<<endl;
+		cout <<endl;
+	}
+	float Display(){
+		Area();
+		Perimeter();
+	}
+};
+int main(){
+	polygon *c;
+	square s;
+	c=&s;
+	c->Display();
+	Rectangle r;
+	c = &r;
+	c->Display();
+	Triangle t;
+	c = &t;
+	c->Display();
+}
+//Question#3
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class sitara
+{
+private:
+	string name;
+	string Rank;
+	int BasicPay;
+	int MD;
+	int HR;
+	int GrossPay;
+	int ann;
+	friend void read_record(sitara );
+	friend int  display(sitara );
+	friend void Gross_pay(sitara );
+	friend void Annual_Increment(sitara );
+	public:
+		
+	sitara(){
+		name = " ";
+		Rank = "";
+		BasicPay = 0;
+		HR = 0;
+	}
+};
+
+void read_record(sitara obj)
+{
+	cout<<"Enter name of empolyee: "<<endl;
+	cin>>obj.name;
+	cout<<"Enter the Rank of employee :"<<endl;
+	cin>>obj.Rank;
+	cout<<"Basic pay of employee: "<<endl;
+	cin>>obj.BasicPay;
+	cout<<"medical allowance: "<<endl;
+	cin>>obj.MD;
+	cout<<"House Rent: "<< endl;
+	cin>>obj.HR;
+	display(obj);
+}
+
+
+int display(sitara obj){
+	
+cout<<"your output:"<< endl;
+	cout<<obj.name<<endl;
+	
+	cout<<obj.Rank<<endl;
+	
+	cout<<obj.BasicPay<<endl;
+	
+	cout<<obj.MD<<endl;
+	
+	cout<<obj.HR<<endl;
+
+Gross_pay(obj);
+}
+void Gross_pay (sitara obj)
+{
+	obj.GrossPay =  obj.BasicPay+ obj.MD;
+	cout<<"Gross pay "<<obj.GrossPay<< endl;
+	Annual_Increment(obj); 
+}
+
+void Annual_Increment (sitara obj)
+{
+	obj.ann=  0.2 * obj.GrossPay;
+	//  ob.ann = ob.ann +ob.GrossPay; 
+	cout<<"increment in salary after 1 year:   "<<obj.ann<<endl;
+}
+
+int main(){
+	
+	sitara obj;
+
+	read_record(obj);
+	display(obj);
+	Gross_pay(obj);
+	Annual_Increment(obj);
+return 0;
+}
